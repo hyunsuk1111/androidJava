04b15044d636352304a25bf8b6cf9f040525aea9
hyunsuk1111 <43610018+hyunsuk1111@users.noreply.github.com>
2020-08-24 18:21:46 +0900

    Add files via upload

--- java04/src/instagram/MyInfoUpdate.java
+++ java04/src/instagram/MyInfoUpdate.java
@@ -7,14 +7,14 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextField;
 import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
-//첫화면 - 로그인
-public class register {
+public class MyInfoUpdate {
 	private static JPasswordField tfPW;
 	private static JPasswordField tfPWC;
-	
+	private static JPasswordField passwordField;
+
 	public static void main(String[] args) {
 		JFrame f = new JFrame("Instagram");
 		ImageIcon img = new ImageIcon("InsLogo.png");
@@ -27,50 +27,59 @@ public class register {
 		
 		JLabel lb2 = new JLabel("아이디");
 		lb2.setFont(new Font("굴림", Font.BOLD, 17));
-		lb2.setBounds(157, 276, 73, 45);
+		lb2.setBounds(148, 322, 73, 45);
 		f.getContentPane().add(lb2);
 		
-		JLabel lb3 = new JLabel("비밀번호");
+		JLabel lb3 = new JLabel("현재 비밀번호");
 		lb3.setFont(new Font("굴림", Font.BOLD, 17));
-		lb3.setBounds(157, 347, 89, 45);
+		lb3.setBounds(148, 404, 125, 45);
 		f.getContentPane().add(lb3);
 		
-		JLabel lb4 = new JLabel("비밀번호 확인");
+		JLabel lb4 = new JLabel("변경할 비밀번호");
 		lb4.setFont(new Font("굴림", Font.BOLD, 17));
-		lb4.setBounds(157, 430, 126, 45);
+		lb4.setBounds(149, 480, 180, 45);
 		f.getContentPane().add(lb4);
 		
 		JLabel lb5 = new JLabel("전화번호");
 		lb5.setFont(new Font("굴림", Font.BOLD, 17));
-		lb5.setBounds(157, 511, 107, 45);
+		lb5.setBounds(150, 628, 107, 45);
 		f.getContentPane().add(lb5);
 		
 		JButton btnRegister = new JButton("회원가입");
 		btnRegister.setFont(new Font("굴림", Font.BOLD, 20));
-		btnRegister.setBounds(181, 659, 168, 38);
+		btnRegister.setBounds(161, 750, 168, 38);
 		f.getContentPane().add(btnRegister);
 		
 		JTextField tfID = new JTextField(10);
 		tfID.setFont(new Font("굴림", Font.PLAIN, 20));
 		tfID.setBackground(Color.WHITE);
 		tfID.setColumns(10);
-		tfID.setBounds(158, 316, 222, 31);
+		tfID.setBounds(150, 355, 202, 31);
 		f.getContentPane().add(tfID);
 		
 		JTextField tfPNum = new JTextField(10);
 		tfPNum.setFont(new Font("굴림", Font.PLAIN, 20));
 		tfPNum.setBackground(Color.WHITE);
 		tfPNum.setColumns(10);
-		tfPNum.setBounds(157, 546, 222, 31);
+		tfPNum.setBounds(150, 662, 202, 31);
 		f.getContentPane().add(tfPNum);
 		
 		tfPW = new JPasswordField();
-		tfPW.setBounds(158, 384, 222, 31);
+		tfPW.setBounds(150, 439, 202, 31);
 		f.getContentPane().add(tfPW);
 		
 		tfPWC = new JPasswordField();
-		tfPWC.setBounds(158, 465, 222, 31);
+		tfPWC.setBounds(150, 516, 202, 31);
 		f.getContentPane().add(tfPWC);
+		
+		JLabel lb4_1 = new JLabel("변경할 비밀번호 확인");
+		lb4_1.setFont(new Font("굴림", Font.BOLD, 17));
+		lb4_1.setBounds(150, 545, 180, 45);
+		f.getContentPane().add(lb4_1);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(151, 581, 201, 31);
+		f.getContentPane().add(passwordField);
 		
 		f.setSize(540,960);
 		f.setVisible(true); //제일 마지막
--- java04/src/instagram/Start.java
+++ java04/src/instagram/Start.java
@@ -8,8 +8,10 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-//첫화면 - 로그인
+//첫화면 -> 로그인 or 회원가입
 public class Start {
 
 	public static void main(String[] args) {
@@ -19,39 +21,30 @@ public class Start {
 		ImageIcon img = new ImageIcon("InsLogo.png");
 		JLabel lb1 = new JLabel(img);
 		lb1.setIcon(img);
-		lb1.setBounds(56, 118, 428, 146);
+		lb1.setBounds(74, 127, 363, 111);
 		f.getContentPane().add(lb1);
-		
-		JLabel lb2 = new JLabel("아이디");
-		lb2.setFont(new Font("굴림", Font.BOLD, 20));
-		lb2.setBounds(220, 309, 84, 33);
-		f.getContentPane().add(lb2);
-		
-		JLabel lb3 = new JLabel("비밀번호");
-		lb3.setFont(new Font("굴림", Font.BOLD, 20));
-		lb3.setBounds(212, 384, 91, 40);
-		f.getContentPane().add(lb3);
-		
-		//텍스트필드2개(아이디/비밀번호)
-		JTextField tfID = new JTextField(10);
-		tfID.setBackground(Color.WHITE);
-		tfID.setBounds(171, 351, 171, 21);
-		JTextField tfPW = new JTextField(15);
-		tfPW.setBounds(171, 432, 171, 21);
 		//버튼(로그인)
 		JButton btnLogin = new JButton("로그인");
-		btnLogin.setBounds(210, 497, 97, 23);
+		btnLogin.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnLogin.setFont(new Font("굴림", Font.BOLD, 20));
+		btnLogin.setBounds(103, 527, 325, 37);
 		f.getContentPane().setLayout(null);
 		
 		
 		f.getContentPane().add(lb1);
-		f.getContentPane().add(tfID);
-		f.getContentPane().add(lb2);
-		f.getContentPane().add(tfPW);
 		f.getContentPane().add(btnLogin);
 		
 		JButton btnNewButton = new JButton("회원가입");
-		btnNewButton.setBounds(210, 463, 97, 23);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setFont(new Font("굴림", Font.BOLD, 20));
+		btnNewButton.setBounds(103, 595, 325, 37);
 		f.getContentPane().add(btnNewButton);
 		
 
